eb077f2e27ded2e2bb85737fc0607b62e1bfde47
leonel-carvajal <liosoft777@gmail.com>
2020-12-10 19:28:35 -0600

    Âfaq

--- script/script.js
+++ script/script.js
@@ -1,9 +1,18 @@
 const ham = document.getElementById('ham')
 const modal = document.getElementById('modal')
 const close = document.getElementById('close')
+const question = document.querySelectorAll('.question')
 ham.addEventListener('click', () => {
   modal.classList.add('show')
 })
 close.addEventListener('click', () => {
   modal.classList.remove('show')
+})
+let arrowup = 'icon-arrow-up.svg';
+let arrowdown = 'icon-arrow.svg';
+question.forEach(el => {
+  el.addEventListener('click', (e) => {
+    el.nextElementSibling.classList.toggle('show-p')
+    el.classList.toggle('arrow')
+  })
 })
